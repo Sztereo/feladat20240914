3100ed432711bcea0d02d52cfc02a8e2b478c3ef
sztehlogabor <sztehlo.gabor@gmail.com>
2024-09-14 10:17:43 +0200

    first commit

---
+++ feladat3/script.js
@@ -0,0 +1,24 @@
+var xhr = new XMLHttpRequest();
+xhr.open("get", "https://www.w3schools.com/xml/cd_catalog.xml");
+xhr.onreadystatechange = function(){
+    if(xhr.readyState == 4 && xhr.status == 200){
+        var feldolgozo = new DOMParser();
+        var xml = feldolgozo.parseFromString(xhr.responseText, "text/xml");
+
+        for(var i = 0; i < xml.children[0].children.length; i++){
+            console.log(xml.children[0].children[i].children);
+
+            var tr = document.createElement("tr");
+
+            for(var j = 0; j < xml.children[0].children[i].children.length; j++){
+                var td = document.createElement("td");
+                td.appendChild(document.createTextNode(xml.children[0].children[i].children[j].innerHTML));
+
+                tr.appendChild(td);
+            }
+
+            document.getElementById("torzs").appendChild(tr);
+        }
+    }
+};
+xhr.send(null);
